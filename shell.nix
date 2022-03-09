afb52ee34a08171ec6df57697448e6260e21e588
Rickard Andersson <gonz@severnatazvezda.com>
2022-03-09 23:32:40 +0200

    dev: add nix files

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  pkgs = import <nixpkgs> { };
+in (import ./default.nix).shellFor {
+  withHoogle = true;
+
+  tools = {
+    cabal = "3.2.0.0";
+    hlint = "latest";
+    haskell-language-server = "latest";
+    ormolu = "0.3.1.0";
+  };
+
+  # All dependencies should be provided by nix
+  exactDeps = true;
+}
