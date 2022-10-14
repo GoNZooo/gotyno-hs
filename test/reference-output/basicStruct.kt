103a523c8b547e9cdf6c0d00cb68047cea256fe3
Rickard Andersson <gonz@severnatazvezda.com>
2022-10-14 19:32:20 +0300

    testing(ts): add test for `basicStruct`

--- test/reference-output/basicStruct.kt
+++ test/reference-output/basicStruct.kt
@@ -6,8 +6,16 @@ import com.fasterxml.jackson.databind.annotation.*
 import com.fasterxml.jackson.databind.*
 import com.fasterxml.jackson.core.*
 import com.fasterxml.jackson.databind.deser.std.*
-/* struct BasicStruct { */
-/*   field1: U32 */
-/*   field2: String */
-/* } */
+import java.text.ParseException
+import java.math.BigInteger
+import kotlinx.serialization.Serializable
 
+class BasicStruct {
+@Serializable
+data class BasicStruct(
+    @get:JsonProperty("field1")
+    val field1: Int,
+    @get:JsonProperty("field2")
+    val field2: String
+) : java.io.Serializable
+}
